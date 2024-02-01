43d29ace8c3310aa0a0cee404d985f6def1c4f6a
harsh <harshpriyam.official@gmail.com>
2024-02-01 16:23:55 +0530

    Data ingestion Stage Created

---
+++ srcx/main.py
@@ -0,0 +1,16 @@
+from VisualSearch import logger
+
+from VisualSearch.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
+
+
+
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+        logger.info(f">>>>> stage {STAGE_NAME} started")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx===========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
